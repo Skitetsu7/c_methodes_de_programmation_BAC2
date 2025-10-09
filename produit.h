6b7feffc44b7b5edf4bc68ab6022b8a42b355fb3
Mohamed Boukhatem <boukhatemmohamed121@gmail.com>
2025-10-09 14:52:44 +0200

    Create produit.h

---
+++ produit.h
@@ -0,0 +1,10 @@
+#ifndef PRODUIT_H
+#define PRODUIT_H
+
+int produit(int *a, int *b);
+
+#endif
+
+// l'appel dans le terminal bash est : gcc tp_2.c -o produit
+// puis ./produit
+// avec le fichier tp_2.c
