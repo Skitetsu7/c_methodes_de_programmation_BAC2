fd9e34b45376521475d1e0421e2ac25e182b4811
Mohamed Boukhatem <boukhatemmohamed121@gmail.com>
2025-10-17 12:06:28 +0200

    Create tp3.c

---
+++ tp3.c
@@ -0,0 +1,31 @@
+// exo 20
+/*libre = global       lié= local
+faut juste donnée les liées
+    point 1 : i
+    point 2 : x
+    point 3 : i
+    point 4: n et k
+    point 5 : y et x
+    
+*/
+// exo 21
+/*
+point 1 : 1,3,1,2,3,3
+point 2 : 1,3,1,2,3,3
+point 3 : 1, 45, 3, 76
+point 4 : 1,76,1,2,5,-1
+point 5 : 4,9
+*/
+// exo 22
+/*
+point 1 : 5,7,8,9
+point 2 : (1,1),(2,2),(3,3),(9,3),(10,2),(1,6),(2,7),(10,7),(1,8)
+point 3 : 1,2,3,9,10
+point 4 : (1,2,3),(2,3,76)
+point 5 : (4,5),(5,6),(7,8), (4,5,6)
+point 6 : ({1,3,5,3},{2,76,2})
+*/
+// exo 23
+/*
+
+*/
